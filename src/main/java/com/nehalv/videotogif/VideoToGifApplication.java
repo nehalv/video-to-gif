6002bf227ee3af354c818c6f63021bbef38f749f
Nehal Vaidya <nehalv@yahoo.com>
2019-04-13 21:35:00 -0500

    reset the author

---
+++ src/main/java/com/nehalv/videotogif/VideoToGifApplication.java
@@ -0,0 +1,15 @@
+package com.nehalv.videotogif;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class VideoToGifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoToGifApplication.class, args);
+	}
+
+}
